ac22f059b9ed850a1417151b75df4400d40865d3
melnyczuk <h.melnyczuk@gmail.com>
2020-02-24 23:04:20 +0000

    :recycle: Simplify uri routing

--- dev/app.ts
+++ dev/app.ts
@@ -6,24 +6,20 @@ const app = express();
 const HOST = process.env.HOST || 'localhost';
 const PORT = parseInt(process.env.PORT || '6666');
 
-app.listen(PORT, HOST, () => console.log('dowsing...'));
+app.listen(PORT, HOST, () => console.log('dummy dowsing...'));
 
-app.get(
-  '/:pkg/:subpkg/:func',
-  async ({ params: { pkg, subpkg, func } }, res) => {
-    const uri = `${__dirname}/data/${pkg}/${subpkg}/${func}.json`;
-    console.log(`mocking request: ${uri}`);
-    try {
-      const data = await fs.readFileSync(uri);
-      res.send(data);
-    } catch (e) {
-      if (e.code === 'ENOENT') {
-        console.log('404', e.path, e.message);
-        res.status(404).send(e);
-      } else {
-        console.log('500', e.message);
-        res.status(500).send(e);
-      }
+app.get('/:pkg/:subpkg/:func', async ({ path }, res) => {
+  console.log(`mocking request: ${path}`);
+  try {
+    const data = await fs.readFileSync(`${__dirname}/data/${path}.json`);
+    res.send(data);
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      console.log('404', e.path, e.message);
+      res.status(404).send(e);
+    } else {
+      console.log('500', e.message);
+      res.status(500).send(e);
     }
-  },
-);
+  }
+});
