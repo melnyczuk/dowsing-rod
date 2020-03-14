80616bdc3058a29ace659f43abd71dd3224614ed
melnyczuk <h.melnyczuk@gmail.com>
2020-03-14 00:26:43 +0000

    Add dev file and route

--- dev/app.ts
+++ dev/app.ts
@@ -23,3 +23,19 @@ app.get('/:pkg/:subpkg/:func', async ({ path }, res) => {
     }
   }
 });
+
+app.get('/:pkg/:func', async ({ path }, res) => {
+  console.log(`mocking request: ${path}`);
+  try {
+    const data = await fs.readFileSync(`${__dirname}/data/${path}.json`);
+    res.send(data);
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      console.log('404', e.path, e.message);
+      res.status(404).send(e);
+    } else {
+      console.log('500', e.message);
+      res.status(500).send(e);
+    }
+  }
+});
