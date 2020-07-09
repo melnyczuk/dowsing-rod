0c36200f261dd36c7039fb7f769729e4e1911b2f
melnyczuk <h.melnyczuk@gmail.com>
2020-07-09 18:20:11 +0100

    Set timeout via .env

--- src/cache.py
+++ src/cache.py
@@ -6,7 +6,7 @@ config = {
     "CACHE_TYPE": "redis",
     "CACHE_REDIS_HOST": os.environ.get("REDIS_HOST", "127.0.0.1"),
     "CACHE_REDIS_PORT": os.environ.get("REDIS_PORT", 6379),
-    "CACHE_DEFAULT_TIMEOUT": 86400,
+    "CACHE_DEFAULT_TIMEOUT": os.environ.get("REDIS_TTL", 86400),
 }
 
 cache = Cache(config=config)
