d9332642d4b290fbf4d43acd6a50b603b5cc9ff3
melnyczuk <h.melnyczuk@gmail.com>
2020-07-09 18:14:45 +0100

    Add cache config

---
+++ src/cache.py
@@ -0,0 +1,12 @@
+import os
+from flask_caching import Cache  # type: ignore
+
+
+config = {
+    "CACHE_TYPE": "redis",
+    "CACHE_REDIS_HOST": os.environ.get("REDIS_HOST", "127.0.0.1"),
+    "CACHE_REDIS_PORT": os.environ.get("REDIS_PORT", 6379),
+    "CACHE_DEFAULT_TIMEOUT": 86400,
+}
+
+cache = Cache(config=config)
