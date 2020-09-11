8a57b9e454e83e1e9a893f2018d65966f440a5e3
how melnyczuk <how@melnycz.uk>
2020-09-11 18:46:53 +0100

    Add logger

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from typing import Any, Tuple
 
 from src.cache import cache
+from src.logger import Logger
+
 from src.google.places.controller import places as google_places_blueprint
 
 app = Flask(__name__)
@@ -9,29 +11,35 @@ app.register_blueprint(google_places_blueprint)
 
 cache.init_app(app)
 cache.set("ping", "pong")
+logger = Logger()
 
 
 @app.route("/ping", methods=["GET"])
 def ping() -> Tuple[Any, int]:
+    logger.request()
     pong = cache.get("ping")
     return (jsonify(pong), 200 if pong == "pong" else 507)
 
 
 @app.errorhandler(400)
 def four_hundred(e: object) -> Tuple[Any, int]:
-    return (jsonify(error=str(e)), 400)
+    logger.request(400)
+    return (jsonify(str(e)), 400)
 
 
 @app.errorhandler(403)
 def four_oh_three(e: object) -> Tuple[Any, int]:
-    return (jsonify(error=str(e)), 403)
+    logger.request(403)
+    return (jsonify(str(e)), 403)
 
 
 @app.errorhandler(404)
 def four_oh_four(e: object) -> Tuple[Any, int]:
-    return (jsonify(error=str(e)), 404)
+    logger.request(404)
+    return (jsonify(str(e)), 404)
 
 
 @app.errorhandler(500)
 def five_hundred(e: object) -> Tuple[Any, int]:
-    return (jsonify(error=str(e)), 500)
+    logger.request(500)
+    return (jsonify(str(e)), 500)
---
+++ src/logger.py
@@ -0,0 +1,12 @@
+from flask import request
+from datetime import datetime
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Logger:
+    def get_time(self: "Logger") -> str:
+        return datetime.now().strftime("%Y/%m/%d-%H:%M:%S")
+
+    def request(self: "Logger", status=200) -> None:
+        print(f"{self.get_time()} {request.path} {status} {request.origin}")
