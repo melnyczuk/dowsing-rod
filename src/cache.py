0e1eaa498c694f968b63891a55f2b5e4a6226d24
Howard Melnyczuk <h.melnyczuk@gmail.com>
2020-09-11 20:36:10 +0100

    🔊 Add logger (#25)
    * Fix cache & env vars
    
    * Add logger
    
    * format lint typecheck refactor

--- src/cache.py
+++ src/cache.py
@@ -4,9 +4,9 @@ from flask_caching import Cache  # type: ignore
 
 config = {
     "CACHE_TYPE": "redis",
-    "CACHE_REDIS_HOST": os.environ.get("REDIS_HOST", "127.0.0.1"),
-    "CACHE_REDIS_PORT": os.environ.get("REDIS_PORT", 6379),
-    "CACHE_DEFAULT_TIMEOUT": os.environ.get("REDIS_TTL", 86400),
+    "CACHE_REDIS_HOST": str(os.environ.get("REDIS_HOST", "127.0.0.1")),
+    "CACHE_REDIS_PORT": int(os.environ.get("REDIS_PORT", "6379")),
+    "CACHE_DEFAULT_TIMEOUT": int(os.environ.get("REDIS_TTL", "86400")),
 }
 
 cache = Cache(config=config)
--- src/google/places/errors.py
+++ src/google/places/errors.py
@@ -1,26 +1,40 @@
 from flask import abort, jsonify
 from typing import Any, Callable, Tuple
 
+from src.logger import Logger
+
 from .models import GoogleException
 
 
-def handle_errors(func: Callable[..., Any]) -> Callable[..., Tuple[Any, int]]:
+logger = Logger()
+
+
+def handle_errors(
+    func: Callable[..., Any], status: int = 200
+) -> Callable[..., Tuple[Any, int]]:
     def fallible_function() -> Tuple[Any, int]:
         try:
             results = func()
+            logger.request(status)
+            return (jsonify(results), status)
+
         except TypeError as e:
+            logger.request(400)
             abort(400, f"Bad input ({e})")
+            return
 
         except GoogleException as e:
+            logger.request(400)
             abort(
-                400, f"Google didn't like something ({e.status}: {e.message})",
+                400,
+                f"Google didn't like something ({e.status}: {e.message})",
             )
             return
 
         except Exception as e:
+            logger.request(500)
             abort(500, e)
-
-        return (jsonify(results), 200)
+            return
 
     fallible_function.__name__ = func.__name__
     return fallible_function
